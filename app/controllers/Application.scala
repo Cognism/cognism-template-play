450aa3c9767c95c2d884f21441c77281886bca5e
stipe <sbuljat@gmail.com>
2016-06-13 14:31:26 +0200

    NEW - API - Status

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,20 @@
 package controllers
 
+import play.api.libs.json.Json
 import play.api.mvc._
 
 class Application extends Controller {
 
   def swagger = Action { request =>
     Ok(views.html.swagger())
+  }
+
+  def status = Action { request =>
+    val payload = Json.obj(
+      "status" -> "OK"
+    )
+
+    Ok(payload)
   }
 
 }
